bf81662b307871179d503fba412beccb6faf655d
Bridget Hillyer <bridget.hillyer@gmail.com>
2014-09-08 13:45:10 -0400

    Remove some unnecessary library calls

--- src/circles/lines.clj
+++ src/circles/lines.clj
@@ -3,20 +3,17 @@
 
 (defn setup []
   (q/frame-rate 30)
-  (q/color-mode :hsb)
-  {:color 0
-   :angle 0})
+  (q/color-mode :rgb)
+  (q/stroke 255 0 0))
 
 (defn draw []
   (q/line 0 0 (q/mouse-x) (q/mouse-y))
   (q/line 200 0 (q/mouse-x) (q/mouse-y))
   (q/line 0 200 (q/mouse-x) (q/mouse-y))
-  (q/line 200 200 (q/mouse-x) (q/mouse-y))
-)
+  (q/line 200 200 (q/mouse-x) (q/mouse-y)))
 
 (q/defsketch hello-lines
   :title "You can see lines"
   :size [500 500]
   :setup setup
-  :draw draw
-  )
+  :draw draw )
