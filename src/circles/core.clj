2c30ee975ed168a5fe572b7cf0530d0677d575f8
Bridget Hillyer <bridget.hillyer@gmail.com>
2014-09-08 13:48:53 -0400

    Replaced core.clj with the default Quil lein template sample app (has comments)

--- src/circles/core.clj
+++ src/circles/core.clj
@@ -3,30 +3,45 @@
             [quil.middleware :as m]))
 
 (defn setup []
+  ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
+  ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
+  ; setup function returns initial state. It contains
+  ; circle color and position.
   {:color 0
    :angle 0})
 
 (defn update [state]
-  (let [{:keys [color angle]} state]
-    {:color (mod (+ color 0.7) 255)
-     :angle (mod (+ angle 0.1) q/TWO-PI)}))
+  ; Update sketch state by changing circle color and position.
+  {:color (mod (+ (:color state) 0.7) 255)
+   :angle (+ (:angle state) 0.1)})
 
 (defn draw [state]
+  ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
+  ; Set circle color.
   (q/fill (:color state) 255 255)
+  ; Calculate x and y coordinates of the circle.
   (let [angle (:angle state)
         x (* 150 (q/cos angle))
         y (* 150 (q/sin angle))]
+    ; Move origin point to the center of the sketch.
     (q/with-translation [(/ (q/width) 2)
                          (/ (q/height) 2)]
+      ; Draw the circle.
       (q/ellipse x y 100 100))))
 
-(q/defsketch circles
+(q/defsketch circles 
   :title "You spin my circle right round"
   :size [500 500]
+  ; setup function called only once, during sketch initialization.
   :setup setup
+  ; update is called on each iteration before draw is called.
+  ; It updates sketch state.
   :update update
   :draw draw
+  ; This sketch uses functional-mode middleware.
+  ; Check quil wiki for more info about middlewares and particularly
+  ; fun-mode.
   :middleware [m/fun-mode])
